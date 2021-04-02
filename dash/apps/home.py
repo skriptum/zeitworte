1c7b59f5dce2a3c5b385a3f127b1afbef841887f
marten <kkx@protonmail.com>
2021-04-03 00:24:04 +0200

    changed some styling

--- dash/apps/home.py
+++ dash/apps/home.py
@@ -7,44 +7,38 @@ import dash_html_components as html
 from apps.app2 import fig1 as karten_fig
 from apps.app3 import fig  as autoren_fig
 
-layout = html.Div( style = {"padding": "5%"}, children = [
-    html.Div(className = "row", children = [
-        #the words
-        html.Div(className = "six columns pretty-container", children = [
-            dcc.Link(href = "/worte", children = [
-                html.H1("Worte", className = "header"),
-            ]),
+layout = html.Div( children = [
+    #the words
+    html.Div(className = "pretty-container centered", children = [
+        dcc.Link(href = "/worte", children = [
+            html.H1("Worte", className = "header"),
         ]),
     ]),
+    
 
     #the maps
-    html.Div(className = "row", children = [
-        html.Div(className = "six columns pretty-container", children = [
-            dcc.Link(href = "/karten", children = [
-                html.H1("Karten", className = "header"),
-                dcc.Graph(figure = karten_fig, 
-                config = {"responsive": False, "displayModeBar": False}),
-            ]),
+    html.Div(className = "pretty-container centered", children = [
+        dcc.Link(href = "/karten", children = [
+            html.H1("Karten", className = "header"),
+            dcc.Graph(figure = karten_fig, 
+            config = {"responsive": False, "displayModeBar": False}),
         ]),
     ]),
 
-    html.Div(className = "row", children = [
-        #the authors
-        html.Div(className = "six columns pretty-container", children = [
-            dcc.Link(href = "/autoren", children = [
-                html.H1("Autoren", className = "header"),
-                dcc.Graph(figure = autoren_fig, 
-                config = {"responsive": False, "displayModeBar": False}),
-            ]),
+    #the authors
+    html.Div(className = "pretty-container centered", children = [
+        dcc.Link(href = "/autoren", children = [
+            html.H1("Autoren", className = "header"),
+            dcc.Graph(figure = autoren_fig, 
+            config = {"responsive": False, "displayModeBar": False}),
         ]),
     ]),
+   
 
-    html.Div(className = "row", children = [
-        #the about
-        html.Div(className = "six columns pretty-container", children = [
-            dcc.Link(href = "/about", children = [
-                html.H1("Über die Seite", className = "header"),
-            ])
-        ]),
+    #the about
+    html.Div(className = "pretty-container centered", children = [
+        dcc.Link(href = "/about", children = [
+            html.H1("Über die Seite", className = "header"),
+        ])
     ]),
 ])
